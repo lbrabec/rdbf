6a42a29026fcc5bfa857ae7fa0ff21ffaa5997e9
Lukas Brabec <lbrabec@redhat.com>
2017-03-07 10:37:09 +0100

    point api_url to prod

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 export var Config = {
-    //API_URL: 'http://taskotron.fedoraproject.org/resultsdb_api/api/v2.0/results',
-    API_URL: 'http://localhost:5001/api/v2.0/results',
+    API_URL: 'http://taskotron.fedoraproject.org/resultsdb_api/api/v2.0/results',
+    //API_URL: 'http://localhost:5001/api/v2.0/results',
     TESTCASES: "https://taskotron.fedoraproject.org/resultsdb_api/api/v2.0/testcases?limit=1000",
     REFRESH: 10000,
 }
